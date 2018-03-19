2a0aba5b670d358a8f6e32bd3fb3d51cb68d2737
naman1919 <ndengre@bestpeers.com>
2018-03-19 19:57:37 +0530

    Sudoku updated

--- Sudokujs.js
+++ Sudokujs.js
@@ -1,8 +1,8 @@
-//var num2 = [];
 var td;
 var i,j,inum,id,id2;
 var num3 = [];
 var idstr = [];
+var d = 0;
 function loadSdk()
 {
 	var tab,row,cell,tr,td,num1;
@@ -28,15 +28,18 @@ function loadSdk()
      num1.setAttribute("maxlength","1");
      num1.setAttribute("id",row+""+cell);
      num1.setAttribute("class",t2+""+t1);
+    // num1.value = row+""+cell;
      td.appendChild(num1);
      tr.appendChild(td);
     }
     tab.appendChild(tr);
 	}
 	document.getElementById("sdk").appendChild(tab);
+  sudokuGenerator();
 }
 function checkWin()
 {
+  timeFunction();
 	for(i = 0 ; i < 9 ; i++)
   {
   	for(j =0 ; j < 9 ; j++)
@@ -49,6 +52,12 @@ function checkWin()
      id = this.id;
      id1 = this.id%10;
      id2 = Math.floor(this.id/10);
+     var count = 0;
+     document.getElementById("p1").innerHTML = "";
+     if((this.value.search(/[1-9]/)<0))
+     {
+      document.getElementById("p1").innerHTML = "Enter only number between 1 to 9";
+     }
      for(var k = 0 ; k < 9 ; k++)
      {
      	  if(id != document.getElementById(k+""+id1).id)
@@ -56,6 +65,7 @@ function checkWin()
      	    if(inum == document.getElementById(k+""+id1).value )
      	    {
      	 	    document.getElementById(id).style.background = "pink";
+            count++;
      	    }
      	  }
       	if(id != document.getElementById(id2+""+k).id)
@@ -63,8 +73,13 @@ function checkWin()
          if(inum == document.getElementById(id2+""+k).value)
      		 {
      		  document.getElementById(id).style.background = "pink";	
+          count++;
      		 }
      	  }
+        if(count < 1)
+        {
+          document.getElementById(id).style.background = "white";
+        }
       }
      var cname = this.className;
      var ids = document.getElementsByClassName(cname);
@@ -80,8 +95,131 @@ function checkWin()
      		 }
      	 }
      }
-    alert(inum);
+   // alert(inum);
      }; 
     }
   }
+}
+function sudokuGenerator()
+{
+ var ran_arr = shuffle([1,2,3,4,5,6,7,8,9]);
+ for(var i = 0 ; i < 9 ; i++)
+ {
+   document.getElementById(0+""+i).value = ran_arr[i];
+ }
+ document.getElementById('10').value = document.getElementById('03').value;
+ document.getElementById('11').value = document.getElementById('04').value;
+ document.getElementById('12').value = document.getElementById('05').value;
+ document.getElementById('13').value = document.getElementById('06').value;
+ document.getElementById('14').value = document.getElementById('07').value;
+ document.getElementById('15').value = document.getElementById('08').value;
+ document.getElementById('16').value = document.getElementById('00').value;
+ document.getElementById('17').value = document.getElementById('01').value;
+ document.getElementById('18').value = document.getElementById('02').value;
+
+ document.getElementById('20').value = document.getElementById('13').value;
+ document.getElementById('21').value = document.getElementById('14').value;
+ document.getElementById('22').value = document.getElementById('15').value;
+ document.getElementById('23').value = document.getElementById('16').value;
+ document.getElementById('24').value = document.getElementById('17').value;
+ document.getElementById('25').value = document.getElementById('18').value;
+ document.getElementById('26').value = document.getElementById('10').value;
+ document.getElementById('27').value = document.getElementById('11').value;
+ document.getElementById('28').value = document.getElementById('12').value;
+
+ document.getElementById('30').value = document.getElementById('21').value;
+ document.getElementById('31').value = document.getElementById('22').value;
+ document.getElementById('32').value = document.getElementById('23').value;
+ document.getElementById('33').value = document.getElementById('24').value;
+ document.getElementById('34').value = document.getElementById('25').value;
+ document.getElementById('35').value = document.getElementById('26').value;
+ document.getElementById('36').value = document.getElementById('27').value;
+ document.getElementById('37').value = document.getElementById('28').value;
+ document.getElementById('38').value = document.getElementById('20').value;
+
+ document.getElementById('40').value = document.getElementById('33').value;
+ document.getElementById('41').value = document.getElementById('34').value;
+ document.getElementById('42').value = document.getElementById('35').value;
+ document.getElementById('43').value = document.getElementById('36').value;
+ document.getElementById('44').value = document.getElementById('37').value;
+ document.getElementById('45').value = document.getElementById('38').value;
+ document.getElementById('46').value = document.getElementById('30').value;
+ document.getElementById('47').value = document.getElementById('31').value;
+ document.getElementById('48').value = document.getElementById('32').value;
+
+ document.getElementById('50').value = document.getElementById('43').value;
+ document.getElementById('51').value = document.getElementById('44').value;
+ document.getElementById('52').value = document.getElementById('45').value;
+ document.getElementById('53').value = document.getElementById('46').value;
+ document.getElementById('54').value = document.getElementById('47').value;
+ document.getElementById('55').value = document.getElementById('48').value;
+ document.getElementById('56').value = document.getElementById('40').value;
+ document.getElementById('57').value = document.getElementById('41').value;
+ document.getElementById('58').value = document.getElementById('42').value;
+
+ document.getElementById('60').value = document.getElementById('51').value;
+ document.getElementById('61').value = document.getElementById('52').value;
+ document.getElementById('62').value = document.getElementById('53').value;
+ document.getElementById('63').value = document.getElementById('54').value;
+ document.getElementById('64').value = document.getElementById('55').value;
+ document.getElementById('65').value = document.getElementById('56').value;
+ document.getElementById('66').value = document.getElementById('57').value;
+ document.getElementById('67').value = document.getElementById('58').value;
+ document.getElementById('68').value = document.getElementById('50').value;
+
+ document.getElementById('70').value = document.getElementById('63').value;
+ document.getElementById('71').value = document.getElementById('64').value;
+ document.getElementById('72').value = document.getElementById('65').value;
+ document.getElementById('73').value = document.getElementById('66').value;
+ document.getElementById('74').value = document.getElementById('67').value;
+ document.getElementById('75').value = document.getElementById('68').value;
+ document.getElementById('76').value = document.getElementById('60').value;
+ document.getElementById('77').value = document.getElementById('61').value;
+ document.getElementById('78').value = document.getElementById('62').value;
+
+ document.getElementById('80').value = document.getElementById('73').value;
+ document.getElementById('81').value = document.getElementById('74').value;
+ document.getElementById('82').value = document.getElementById('75').value;
+ document.getElementById('83').value = document.getElementById('76').value;
+ document.getElementById('84').value = document.getElementById('77').value;
+ document.getElementById('85').value = document.getElementById('78').value;
+ document.getElementById('86').value = document.getElementById('70').value;
+ document.getElementById('87').value = document.getElementById('71').value;
+ document.getElementById('88').value = document.getElementById('72').value;
+ 
+ for(i = 0 ; i < 9 ; i++)
+ {
+   for(j = 0 ; j < 9 ; j++)
+   {
+     document.getElementById(Math.floor((Math.random()*9))+''+Math.floor((Math.random()*9))).value = ""; 
+   }
+ }
+ for(i = 0 ; i < 9 ; i++)
+ {
+   for(j = 0 ; j < 9 ; j++)
+   {
+     var disid = document.getElementById(i+""+j);
+     if(disid.value != '')
+     {
+     // alert(disid.id);
+      disid.disabled = true;
+     }
+    }
+  }
+}
+function shuffle(array) 
+{
+  var i = array.length,j = 0,temp;
+   while (i--) {
+     j = Math.floor(Math.random() * (i+1));
+     temp = array[i];
+     array[i] = array[j];
+     array[j] = temp;
+    }
+   return array;
+}
+function timeFunction()
+{
+  document.getElementById("time").innerHTML=d++;
+  setTimeout(timeFunction,1000);
 }
